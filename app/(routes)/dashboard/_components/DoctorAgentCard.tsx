34ed90ea51420361bbfb8579e09e7b2aba069ff0
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2025-10-08 17:19:55 +0300

    Add doctor agents

--- app/(routes)/dashboard/_components/DoctorAgentCard.tsx
+++ app/(routes)/dashboard/_components/DoctorAgentCard.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Image from 'next/image';
+import { IconArrowRight } from '@tabler/icons-react';
+import { Button } from '@/components/ui/button';
 
 type doctorAgent={
     id:number,
@@ -13,9 +15,10 @@ function DoctorAgentCard({ agent }: { agent: doctorAgent }) {
   return (
     <div className=''>
       <Image src={agent.image} alt={agent.specialist} width={200} height={300} className='w-full h-[250px] object-cover rounded-xl'/>
-       <h2 className='font-bold'>{agent.specialist}</h2>
+       <h2 className='font-bold mt-1'>{agent.specialist}</h2>
       <p className='line-clamp-2 text-sm text-gray-500'>{agent.description}</p>
       {/* <p className='text-sm text-gray-500'>{agent.agentPrompt}</p> */}
+      <Button className='w-full mt-2'>Start Consultation <IconArrowRight /></Button>
     </div>
   )
 }
--- app/(routes)/dashboard/_components/DoctorsAgentList.tsx
+++ app/(routes)/dashboard/_components/DoctorsAgentList.tsx
@@ -7,7 +7,7 @@ function DoctorsAgentList() {
     return (
         <div className='mt-10'>
             <h2 className='font-bold text-xl'>AI Specialist Doctors Agent</h2>
-            <ul className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mt-5 gap-4'>
+            <ul className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mt-5 gap-8'>
                 {AIDoctorAgents.map((agent) => (
                     <div key={agent.id}>
                         {/* <Image src={agent.image} alt={agent.specialist} width={50} height={50} className='rounded-full'/>
