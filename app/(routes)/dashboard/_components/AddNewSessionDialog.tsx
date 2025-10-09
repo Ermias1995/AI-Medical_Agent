81089314f4aea13a9f4febb63bf3279bac5e4aa7
Ermias1995 <ermias.firdawok-ug@aau.edu.et>
2025-10-09 12:04:21 +0300

    Add symptom and note

--- app/(routes)/dashboard/_components/AddNewSessionDialog.tsx
+++ app/(routes)/dashboard/_components/AddNewSessionDialog.tsx
@@ -1,31 +1,45 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Button } from '@/components/ui/button'
 import { IconArrowRight } from '@tabler/icons-react';
+import { Textarea } from '@/components/ui/textarea';
+import { ArrowRight } from 'lucide-react';
 
 function AddNewSessionDialog(){
+    const [note, setNote] = useState<string>(); 
     return (
         <Dialog>
         <DialogTrigger>
-            <Button className='w-full mt-2'>Start Consultation <IconArrowRight /></Button>
+            <Button className='mt-3'> + Start a Consultation</Button>
         </DialogTrigger>
         <DialogContent>
             <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
-                This action cannot be undone. This will permanently delete your account
-                and remove your data from our servers.
+            <DialogTitle>Add Basic Details</DialogTitle>
+            <DialogDescription asChild>
+                <div>
+                    <h2>Add Symptoms or Any other Details</h2>
+                    <Textarea placeholder='Add Detail here...' className='h-[200px] mt-1' onChange={(e)=>setNote(e.target.value)}/>
+                </div>
             </DialogDescription>
             </DialogHeader>
+            <DialogFooter>
+                <DialogClose>
+                    <Button variant={'outline'}>Cancel</Button>
+                </DialogClose>
+                <Button disabled={!note}>Next <ArrowRight/> </Button>
+            </DialogFooter>
         </DialogContent>
-        </Dialog>
+        </Dialog> 
     )
 }
 
--- app/(routes)/dashboard/_components/HistoryList.tsx
+++ app/(routes)/dashboard/_components/HistoryList.tsx
@@ -2,6 +2,7 @@
 import {useState} from 'react'
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
+import AddNewSessionDialog from './AddNewSessionDialog';
 
 const HistoryList = () => {
     const [historyList, setHistoryList] = useState([]);
@@ -13,7 +14,7 @@ const HistoryList = () => {
                 <Image src={'/medical-assistance.png'} alt='empty' width={200} height={200} />
                 <h2 className='font-bold text-xl mt-2'>No history available</h2>
                 <p>It looks like you haven't interacted with the medical assistant yet.</p>
-                <Button className='mt-3'> + Start a Consultation</Button>
+                <AddNewSessionDialog />
             </div>:
             <div>List</div>}
         </div>
